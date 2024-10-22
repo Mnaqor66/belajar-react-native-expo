f633a2c11ea7649c480579e39a9f3572e415e2bb
Muhammad Naufal Abqori <naufalabqori55@gmail.com>
2024-10-22 10:27:34 +0800

    config splash screen and app icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,8 @@
+import * as SplashScreen from 'expo-splash-screen';
 import { Stack } from "expo-router";
+
+SplashScreen.preventAutoHideAsync()
+setTimeout(SplashScreen.hideAsync, 5000)
 
 export default function RootLayout() {
   return (
